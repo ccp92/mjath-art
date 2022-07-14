5c24ad3e9b8a01539de66608433a26607b5ce52e
ccp92 <mail@chrisparsons.dev>
2022-07-14 23:08:03 +0100

    Strip out unnecessary pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <Seo title="About" />
+      <h1>About</h1>
+      <p>This is the about page</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
